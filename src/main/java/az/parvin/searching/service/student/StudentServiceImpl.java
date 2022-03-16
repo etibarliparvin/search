ddc151ba793862ff3e8bcfe0972bcdabc52fdf04
etibarliparvin <etibarliparvin@gmail.com>
2022-03-16 22:38:52 +0400

    .

--- src/main/java/az/parvin/searching/service/student/StudentServiceImpl.java
+++ src/main/java/az/parvin/searching/service/student/StudentServiceImpl.java
@@ -1,11 +1,17 @@
 package az.parvin.searching.service.student;
 
+import az.parvin.searching.dto.response.StudentResponse;
+import az.parvin.searching.entity.Student;
 import az.parvin.searching.exception.common.CommonResponse;
 import az.parvin.searching.mapper.StudentMapper;
 import az.parvin.searching.repository.StudentRepository;
 import az.parvin.searching.search.student.SearchFilter;
+import az.parvin.searching.search.student.SearchSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +23,12 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public CommonResponse search(SearchFilter filter) {
-        return null;
+        CommonResponse response = new CommonResponse();
+        SearchSpecification searchSpecification = new SearchSpecification();
+        searchSpecification.search(filter);
+        List<Student> students = studentRepository.findAll(searchSpecification);
+        List<StudentResponse> studentResponse = students.stream().map(studentMapper::toResponse).collect(Collectors.toList());
+        response.setItem(studentResponse);
+        return response;
     }
 }
